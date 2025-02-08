191fb1a6ff93cf2feb8e729fe701423187403df6
Keabetsweselepe1 <158192880+Keabetsweselepe1@users.noreply.github.com>
2025-02-08 13:41:21 +0200

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight',
-  author: 'kea',
-  botName: "Kea Bot",
-  botOwner: 'Keabetswe', // Your name
-  ownerNumber: '+27692246638', //Your number
+  author: 'Bot',
+  botName: "kea bot",
+  botOwner: 'keabetswe', // Your name
+  ownerNumber: '0692246638', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
-};keabetswe
+};
 
 module.exports = settings;
