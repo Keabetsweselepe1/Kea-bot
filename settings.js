5f7b8ceb6d06efbdafc914eb7bd28a7e6bfa5988
Keabetsweselepe1 <158192880+Keabetsweselepe1@users.noreply.github.com>
2025-02-08 11:13:35 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'bot',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Kea Bot",
+  botOwner: 'keabetswe', // Your name
+  ownerNumber: '+2769224663',//Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
-};
+}; status view
 
 module.exports = settings;
